73ee116f406ed0fc070cff17b945781f66ece7fe
Ripeplantain <irvingmanny@gmail.com>
2022-06-02 19:20:39 +0000

    betty you are trash

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
 
 /**
- * 
- * * main - main block function
- * 
- * * Return: 0 return 0 if code passes
- *
- *  */
-
- int main(void)
-
- {
-	
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
- }
+}
