10e58a8a6e56eaed7d5cca75625c47c270d8fd6b
Ripeplantain <irvingmanny@gmail.com>
2022-06-02 16:47:10 +0000

    fixed output

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -10,6 +10,6 @@
 
 int main(void)
 {
-	printf("wth proper grammer,but the outcome is a piece of art");
-	return (0);
+	 printf("with proper grammar, but the outcome is a piece of art,\n");
+	 return (0);
 }
