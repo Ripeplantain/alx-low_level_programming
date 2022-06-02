7ed71837e430aae3b96b7740b0be8021b7da0457
Ripeplantain <irvingmanny@gmail.com>
2022-06-02 19:18:15 +0000

    this is my last try betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - prints the size of various types
- * Return: 0 if exited properly, non-zero otherwise
- */
+ * 
+ * * main - main block function
+ * 
+ * * Return: 0 return 0 if code passes
+ *
+ *  */
 
-int main(void)
-{
+ int main(void)
+
+ {
+	
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
-}
+ }
