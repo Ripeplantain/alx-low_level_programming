7014a8d139d7b9e04f341c3ad94f7db6ee766953
Ripeplantain <irvingmanny@gmail.com>
2022-06-02 16:43:20 +0000

    this is for the print function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - This prints file into header file
+ *@void : Arguement for main function
+ *Description : This function is used to print,
+ *what is in the function.
+ * return : This return int 0 if codes success
+ */
+
+int main(void)
+{
+	printf("wth proper grammer,but the outcome is a piece of art");
+	return (0);
+}
